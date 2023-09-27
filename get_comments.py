2ae6eccfdfe4d347edf800c4049331f3a30af613
Jooyoung Jeong <jooyoungworknd@gmail.com>
2023-09-27 13:40:10 +0900

    initialize repository

---
+++ get_comments.py
@@ -0,0 +1,67 @@
+import requests
+import os
+import json
+
+API_KEY = 'AIzaSyCnU1SN56rFEdmccXhyjXmSh5zAvUHq3K0'
+API_COMMENTTHREAD_REQ = 'https://www.googleapis.com/youtube/v3/commentThreads'
+VIDEOID = '_XTGPe0Ui_g'
+
+comment_params = {
+    'key':API_KEY,
+    'videoId' : VIDEOID,
+    'part' : 'snippet,replies',
+    'maxResults' : '100', 
+}
+
+def get_video_data(vid):
+    
+    comment_params['key'] = API_KEY
+    comment_params['videoId'] = vid
+    
+    while True:
+        res = requests.get(API_COMMENTTHREAD_REQ, params=comment_params)
+        if res.status_code != 200:
+            del comment_params["pageToken"]
+            break
+        data = json.loads(res.text)
+        print(data.keys())
+        if "nextPageToken" in data.keys():
+            comment_params["pageToken"] = data["nextPageToken"]
+        else:
+            del comment_params["pageToken"]
+            break
+        
+        texts = []
+        
+        comment_id = 0
+        for i in data['items']:
+            comment = {
+                "vid": vid,
+                "id": f"{comment_id}",
+                "is_reply":0,
+                "text": i['snippet']["topLevelComment"]["snippet"]["textOriginal"]
+            }
+            texts.append(comment)
+            if i["snippet"]["totalReplyCount"]:
+                reply_id = 0
+                if 'replies' in i.keys():
+                    for j in i['replies']["comments"]:
+                        reply = {
+                            "vid": vid,
+                            "id":f"{comment_id}_{reply_id}",
+                            "is_reply":1,
+                            "text": j['snippet']['textOriginal']
+                        }
+                        texts.append(reply)
+                    
+
+
+url = 'https://www.youtube.com/watch?v=AXSBlElPUpQ'
+vid = url.split('=')[-1]
+
+with open('textfile.txt') as f:
+    lst = f.read().split("\n")
+    
+for url in lst:
+    vid = url.split('=')[-1]
+    get_video_data(vid)
---
+++ get_transcript.py
@@ -0,0 +1,36 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import youtube_transcript_api
+
+VIDEOID = 'dFRXcYJVfMM'
+
+def check_transcript_list(vid):
+    '''
+        vid: videoid
+        
+        this function checks all available transcript from the video corresponding vid
+    '''
+    print(YouTubeTranscriptApi.list_transcripts(vid))
+
+def get_transcripts(vid, *langs):
+    '''
+        vid: videoid
+        *langs: language list
+        
+        caution: this function only collects manual transcription from the video corresponding vid.
+    '''
+    transcript_list =  YouTubeTranscriptApi.list_transcripts(vid)
+    d = {}
+    for lang in langs:
+        try:
+            transcript = transcript_list.find_manually_created_transcript([lang])
+            transcriptdata = transcript.fetch()
+        except youtube_transcript_api.NoTranscriptFound as e:
+            print("the video does not contain {lang} transcription")
+            continue        
+        texts = [timedtranscript['text'].replace('\n', ' ') for timedtranscript in transcriptdata]
+        d[lang] = texts
+    return d
+
+d = get_transcripts(VIDEOID, 'ko', 'en')
+print(d['ko'])
+print(d['en'])
